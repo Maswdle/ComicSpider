835394dcbb0b0fdc77395de589388e6e45ca2fe2
Maswdle <wondr009@163.com>
2022-08-31 11:24:38 +0800

    创建文件和修改工作目录有问题

--- hen.py
+++ hen.py
@@ -19,9 +19,11 @@ class Spider:
     group_pages = 0
     __r_url = ""
     information_dic = {
-        "name": "",
+        "title": "",
         "pages": 0,
-        "language": ""
+        "language": "",
+        "s": 0,
+        "number": 0
     }
 
     # ! 这个是模板
@@ -50,51 +52,73 @@ class Spider:
         soup = BeautifulSoup(cont, "lxml")
         self._getpage_group(soup)
         self._get_all_d()
-        print(len(self.d_lst))
+        print(f'total : {len(self.d_lst)}\n')
 
     def _add_d_lst(self, tags):
         for i in tags:
             self.d_lst.append(i["href"])
 
-    def language_filter(self, language):
-        # todo 完善筛选
-        # * 即语言和标题的获取，下载函数的完善
+    def filter_and_download(self, language):
+        # * 即语言和标题的获取，
         for i in self.d_lst:
             web = requests.get(url=i)
             cont = web.content
             soup = BeautifulSoup(cont, "lxml")
+            # * 语言和标题
+            title = soup.find("title").string
+            lang = soup.find_all("a", class_="name", href=f"https://3hentai.net/language/{language}")
+            if len(lang) == 0:  # 筛选语言
+                continue
+            # 其他
             tag = soup.find("a", class_="name", rel="nofollow")
             s_tags = soup.find_all("img", class_="lazy small-bg-load")
             s_url = s_tags[0].attrs["data-src"]
-            s = re.search(r'/(.+).3', str(s_url)).group()[3]
+            s_number = int(re.search(r'/(.+).3', str(s_url)).group()[3])
             a = str(tag.contents)
             result = re.search(r'\d+', a)
+            ur: str = i
+            d_num = int(ur.split("/")[-1])
+            # * dic
             self.information_dic["pages"] = int(result.group())
+            self.information_dic["s"] = s_number
+            self.information_dic["title"] = title
+            self.information_dic["number"] = d_num
+            self._download()
 
     def parse_url_d(self):
-        for i in self.d_lst:
-            cont = requests.get(i).content
+        ...
 
     def _download(self):
         """
         :f: 下载
         :return: None
+        s_number
         """
-        # todo 更正循环以及及其他
-        os.makedirs(f'./works/{self.information_dic["name"]}', exist_ok=True)
-        os.chdir(f'{os.getcwd()}\\works\\{self.information_dic["name"]}')
-        rl = len(self.image_lst)
-        for i in range(rl):
-            con = requests.get(i).content
+        # todo 创建文件和修改工作目录有问题
+
+        os.makedirs(f'{os.path.abspath(".")}\\works\\{self.information_dic["title"]}', exist_ok=True)
+        os.chdir(f'{os.path.abspath(".")}\\works\\{self.information_dic["title"]}')
+        # ! 上面这两句有问题
+        rl = self.information_dic["pages"]
+        self._out()
+        for i in range(1, rl + 1):
+            url = f'https://s{self.information_dic["s"]}.3hentai.net/d{self.information_dic["number"]}/{i}.jpg'
+            con = requests.get(url=url).content
             fname = f"{i}{self.lastname}"
             with open(fname, "wb") as f:
                 f.write(con)
                 f.close()
-            print(f"{i + 1}/{rl} OK")
+            print(f"{i}/{rl} OK")
         print("end")
+
+    def _out(self):
+        print(f'title: {self.information_dic["title"]}')
+        print(f'pages: {self.information_dic["pages"]}')
+        print(f'number: {self.information_dic["number"]}')
 
 
 if __name__ == "__main__":
     s = Spider("https://3hentai.net/artists/yd")
     s.parse_url_group()
+    s.filter_and_download("chinese")
     print(s.group_pages)
